6cf83a46e562ec6625e20ea25b103e3e31109817
Niels <nielsnickens@gmail.com>
2025-08-05 18:16:59 -0600

    finsished workshop

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const flavors = ["vanilla", "chocolate", "strawberry", "mint", "coffee"];
-let orders = prompt("Enter your order");
+// const flavors = ["vanilla", "chocolate", "strawberry", "mint", "coffee"];
+
+// let order = prompt("Enter your order, options are:" + " " + flavors);
+
+const order = prompt(
+  "Enter your order, seperated by commas only, options are:",
+  "vanilla, chocolate, strawberry, mint, coffee"
+);
+
+const flavors = order.split(",");
+
+const counts = count(flavors);
+
+console.table(counts);
+
+function count(flavors) {
+  const counts = {};
+  for (const flavor of flavors) {
+    if (flavor in counts) {
+      counts[flavor]++;
+    } else {
+      counts[flavor] = 1;
+    }
+  }
+  return counts;
+}
